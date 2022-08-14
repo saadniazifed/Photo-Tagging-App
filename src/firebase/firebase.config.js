9b73f6eb7821cfdb2c4ff96880785583b26622a0
Saad Niazi <saadniazifed@gmail.com>
2022-08-14 17:13:28 +0500

    Added an env file.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "process.env.REACT_APP_APIKEY",
+  authDomain: "process.env.REACT_APP_AUTHDOMAIN",
+  projectId: "process.env.REACT_APP_PROJECTID",
+  storageBucket: "process.env.REACT_APP_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_MESSAGING_SENDER_ID",
+  appId: "process.env.REACT_APP_APP_ID",
+};
