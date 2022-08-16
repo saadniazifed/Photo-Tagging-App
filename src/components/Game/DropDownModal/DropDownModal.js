5904276bd2bcee97664c2b6cbc781d60391c4073
Saad Niazi <saadniazifed@gmail.com>
2022-08-16 16:15:18 +0500

    Made the dropdown modal without any functionality.

--- src/components/Game/DropDownModal/DropDownModal.js
+++ src/components/Game/DropDownModal/DropDownModal.js
@@ -8,28 +8,58 @@ import Tom from "../../../media/findCharacters/Tom.png";
 export const DropDownModal = () => {
   return (
     <>
-      <ul className="dropdown--modal">
-        <li className="images--div">
-          <img src={Chris} alt="chris-family-guy" />
-          <p>Chris</p>
-        </li>
-        <li className="images--div">
-          <img src={Dog} alt="brian-griffin-family-guy" />
-          <p>Brian</p>
-        </li>
-        <li className="images--div">
-          <img src={Kratos} alt="Kratos" />
-          <p>Kratos</p>
-        </li>
-        <li className="images--div">
-          <img src={Link} alt="Link" />
-          <p>Link</p>
-        </li>
-        <li className="images--div">
-          <img src={Tom} alt="Tom" />
-          <p>Tom</p>
-        </li>
-      </ul>
+      <div className="dropdown--modal">
+        <div class="Parent">
+          <div class="child1">
+            <img src={Chris} alt="Chris-family-guy" />
+          </div>
+          <div class="child2">
+            <p>Chris</p>
+          </div>
+        </div>
+
+        <div class="Parent">
+          <div class="child1">
+            <img src={Dog} alt="Griffin-family-guy" />
+          </div>
+          <div class="child2">
+            <p>Griffin</p>
+          </div>
+        </div>
+
+        <div class="Parent">
+          <div class="child1">
+            <img src={Kratos} alt="Kratos" />
+          </div>
+          <div class="child2">
+            <p>Kratos</p>
+          </div>
+        </div>
+
+        <div class="Parent">
+          <div class="child1">
+            <img src={Link} alt="Kratos" />
+          </div>
+          <div class="child2">
+            <p>Link</p>
+          </div>
+        </div>
+
+        <div class="Parent">
+          <div class="child1">
+            <img src={Tom} alt="Kratos" />
+          </div>
+          <div class="child2">
+            <p>Tom</p>
+          </div>
+        </div>
+
+        <button className="cancel--btn">
+          <svg xmlns="" width="14" height="14" viewBox="0 0 24 24">
+            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"></path>
+          </svg>
+        </button>
+      </div>
     </>
   );
 };
