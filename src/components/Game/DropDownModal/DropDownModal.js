7366e2f349f3c766db5c12022a3bb01692c221e2
Saad Niazi <saadniazifed@gmail.com>
2022-08-17 13:19:50 +0500

    Pushing the latest changes.

--- src/components/Game/DropDownModal/DropDownModal.js
+++ src/components/Game/DropDownModal/DropDownModal.js
@@ -5,56 +5,66 @@ import Kratos from "../../../media/findCharacters/Kratos.png";
 import Link from "../../../media/findCharacters/Link.png";
 import Tom from "../../../media/findCharacters/Tom.png";
 
-export const DropDownModal = () => {
+export const DropDownModal = (props) => {
+  const dropDownHide = props.dropDownHide;
+  const leftCord = props.leftCord;
+  const topCord = props.topCord;
+
   return (
     <>
-      <div className="dropdown--modal">
-        <div class="Parent">
-          <div class="child1">
+      <div
+        className="dropdown--modal"
+        style={{
+          top: topCord,
+          left: leftCord,
+        }}
+      >
+        <div className="Parent">
+          <div className="child1">
             <img src={Chris} alt="Chris-family-guy" />
           </div>
-          <div class="child2">
+          <div className="child2">
             <p>Chris</p>
           </div>
         </div>
 
-        <div class="Parent">
-          <div class="child1">
+        <div className="Parent">
+          <div className="child1">
             <img src={Dog} alt="Griffin-family-guy" />
           </div>
-          <div class="child2">
+          <div className="child2">
             <p>Griffin</p>
           </div>
         </div>
 
-        <div class="Parent">
-          <div class="child1">
+        <div className="Parent">
+          <div className="child1">
             <img src={Kratos} alt="Kratos" />
           </div>
-          <div class="child2">
+          <div className="child2">
             <p>Kratos</p>
           </div>
         </div>
 
-        <div class="Parent">
-          <div class="child1">
+        <div className="Parent">
+          <div className="child1">
             <img src={Link} alt="Kratos" />
           </div>
-          <div class="child2">
+          <div className="child2">
             <p>Link</p>
           </div>
         </div>
 
-        <div class="Parent">
-          <div class="child1">
+        <div className="Parent">
+          <div className="child1">
             <img src={Tom} alt="Kratos" />
           </div>
-          <div class="child2">
+          <div className="child2">
             <p>Tom</p>
           </div>
         </div>
 
-        <button className="cancel--btn">
+        <button className="cancel--btn--dropdown" onClick={dropDownHide}>
           <svg xmlns="" width="14" height="14" viewBox="0 0 24 24">
             <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"></path>
           </svg>
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,8 +3,31 @@ import { DropDownModal } from "./DropDownModal/DropDownModal";
 import "./Game.css";
 import { HowToPlay } from "./HowToPlay/HowToPlay";
 import { Leaderboard } from "./Leaderboard/Leaderboard";
+import { useState } from "react";
 
 export const Game = (props) => {
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const dropDownVisible = () => {
+    setShowDropDown(true);
+  };
+
+  const dropDownHide = () => {
+    setShowDropDown(false);
+  };
+
+  const topCord = (e) => {
+    const y = e.offsetY;
+    console.log(y);
+    return y;
+  };
+
+  const leftCord = (e) => {
+    const x = e.offsetX;
+    console.log(x);
+    return x;
+  };
+
   const showHowToPlay = props.showHowToPlay;
   const howToPlayHide = props.howToPlayHide;
 
@@ -15,8 +38,24 @@ export const Game = (props) => {
     <div>
       {showHowToPlay && <HowToPlay howToPlayHide={howToPlayHide} />}
       {showLeaderboard && <Leaderboard leaderboardHide={leaderboardHide} />}
-      <img className="bg-image" src={backgroundImage} alt="background-logo" />
-      <DropDownModal />
+
+      <img
+        className="bg-image"
+        src={backgroundImage}
+        alt="background-logo"
+        onClick={(e) => {
+          dropDownVisible();
+          topCord(e);
+          leftCord(e);
+        }}
+      />
+      {showDropDown && (
+        <DropDownModal
+          dropDownHide={dropDownHide}
+          leftCord={leftCord}
+          topCord={topCord}
+        />
+      )}
     </div>
   );
 };
